4cbe1cddf84e3e29cdfac2a3f1c0a69f7465fe92
202323 <202323@K1210-11>
2026-01-30 09:30:53 +0300

    Добавьте файлы проекта.

---
+++ calculatorForQA/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace calculatorForQA
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
